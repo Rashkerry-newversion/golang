2d651cc521bf9ed699b4ece2d9432fda97980505
Rashkerry-newversion <rashidamohammed666@gmail.com>
2025-09-30 10:57:06 +0000

    first practice commit

--- go-url-shortener/cmd/server/main.go
+++ go-url-shortener/cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/shorten", handlers.ShortenHandler)
+	http.HandleFunc("/", handlers.RedirectHandler)
 
 	fmt.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
